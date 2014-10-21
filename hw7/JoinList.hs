4c2e2657d1178941c7849e9df0b425786b2dd552
mioalter <mioalter@gmail.com>
2014-10-21 08:58:29 -0700

    added note to self

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -1,4 +1,7 @@
 
+-- Unrelated exercise: writer splitter function for balancing a dataset in haskell.
+-- see splitter.hs
+
  data JoinList m a = Empty
                    | Single m a
                    | Append m (JoinList m a) (JoinList m a)
