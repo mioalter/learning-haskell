0f74f31346ccd8b941d861bb80abee95e3cfa5de
mioalter <mioalter@gmail.com>
2015-08-19 10:19:50 -0700

    works, cleaned up some, could use more

--- scripts/splitter.hs
+++ scripts/splitter.hs
@@ -4,7 +4,7 @@ Given a TSV of data with one column (the last) whose values are 'train' and 'tes
 write a script to split the file in two.
 
 
-to list installed pakcages
+to list installed packages
 mioss-mbp-3:scripts mio6si$ ghc-pkg list
 so we have the cmdargs package
 
@@ -46,32 +46,46 @@ outName f s = a ++ "_" ++ s ++ b
 splitHelper :: ([String],[String]) -> String -> ([String],[String])
 splitHelper (tr, ts) s
 	| lst == Just "train" = (s:tr, ts)
-	| otherwise	= (tr, s:ts)
+	| lst == Just "test" = (tr, s:ts)
+	| otherwise	= (tr, ts)
 	where lst = getLast $ wordsBy (=='\t') s
 
 splitsky :: [String] -> ([String], [String])
 splitsky = foldl splitHelper ([],[])
 
--- use wordsBy to split a string on a given character
--- eg. wordsBY (=='\t') "yes\tno\tmaybe"
--- > ["yes","no","maybe"]
--- there is also splitOn which takes a list of delimiters.
--- now we have to figure out how to do the IO part.
+{-
+Note: using foldr preserves the order of the lines in the input file
+whereas using foldl reverses them BUT foldr will build up a thunk
+the size of the input file while foldl will process as it goes
+(foldl is tail-recursive) so foldl is actually what you want to use
+
+(note that the arguments to the folding functions for foldr and foldl
+are reversed: for foldl, the accumulator comes first)
+
+splitHelper :: String -> ([String],[String]) -> ([String],[String])
+splitHelper s (tr, ts)
+	| lst == Just "train" = (s:tr, ts)
+	| otherwise	= (tr, s:ts)
+	where lst = getLast $ wordsBy (=='\t') s
+
+splitsky :: [String] -> ([String], [String])
+splitsky = foldr splitHelper ([],[])
+-}
+
+pipeLine :: [String] -> (String, String)
+pipeLine xs = (unlines $ header : trainLines, unlines $ header : testLines)
+	where 
+		header = head xs
+		(trainLines, testLines) = splitsky xs
 
 main :: IO()
 main = do 
 	opts <- cmdArgs options
-	putStrLn $ "Gonna read " ++ (inFile opts)
 	allLines <- fmap lines $ readFile $ inFile opts
-	--let (trainLines, testLines) = fmap unlines $ splitsky allLines
-	--let (trainName, testName) = (outName f "train", outName f "test") where f = inFile opts
-	let outlines = fmap unlines $ splitsky allLines
-	let outnames = (outName f "train", outName f "test") where f = inFile opts
-	-- let pairs = [(fst outnames, fst outlines),(snd outnames, snd outlines)]
-	putStrLn $ head $ allLines
-	putStrLn $ fst outnames
-	putStrLn $ snd outnames
-	putStrLn $ fst outlines
-	-- why is outlines :: ([String], String) ?? Should be (String, String) because we fmapped unlines.
+	let (trainUnlines, testUnlines) = pipeLine allLines
+	let (trainName, testName) = (outName f "train", outName f "test") where f = inFile opts
+	writeFile trainName trainUnlines
+	writeFile testName testUnlines
+
 	
 
